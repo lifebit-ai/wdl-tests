8bee076ee0e3f9be35c1e085dbdd55544155ae85
Eva Gradovich <eva@macbook-pro.home>
2021-06-15 15:42:29 +0100

    {EG} Implemented changes from PR review

--- gatk-wdl/run_commands.sh
+++ gatk-wdl/run_commands.sh
@@ -1,4 +1,5 @@
 wget https://github.com/broadinstitute/cromwell/releases/download/33.1/cromwell-33.1.jar # grabs a cromwell instance
+wget https://github.com/broadinstitute/cromwell/releases/download/57/cromwell-57.jar
 
 git clone https://github.com/gatk-workflows/seq-format-validation.git
 
@@ -13,5 +14,18 @@ gsutil cp gs://gatk-test-data/wgs_bam/NA12878_24RG_hg38/NA12878_24RG_med.hg38.ba
 
 sed 's?gs:\/\/gatk-test-data\/wgs_bam\/NA12878_24RG_hg38?'`pwd`'?' seq-format-validation/validate-bam.inputs.json > inputs/input.json
 
+##############################################
+# Testing with smaller bams to improve runtime
+##############################################
 
+mkdir small_inputs
+wget  https://lifebit-featured-datasets.s3-eu-west-1.amazonaws.com/IGV/crg-covid/BAM/PRJEB33797_WT.Coronavirus_229E.Spliced_Nanopreprocess_alignment_PRJEB33797_Viehweger_GR2019.minimap2.sorted.bam -P small_inputs/
+wget https://lifebit-featured-datasets.s3-eu-west-1.amazonaws.com/IGV/crg-covid/BAM/8F6N9_Korea_VeroInfected.hg38_NanoPreprocess_alignment_Vero_Infected.minimap2.sorted.bam -P small_inputs/
+
+echo "{
+\"ValidateBamsWf.bam_array\":" > inputs.json
+ls -d ${PWD}/small_inputs/* | jq -R -s -c 'split("\n")[:-1]' >> inputs.json
+echo "}" >> inputs.json
+
+java -jar cromwell-57.jar run ./seq-format-validation/validate-bam.wdl --inputs inputs.json
 java -jar cromwell-33.1.jar run ./seq-format-validation/validate-bam.wdl --inputs inputs/input.json # producess cromwell-executions and cromwell-workflow-logs in current directory
